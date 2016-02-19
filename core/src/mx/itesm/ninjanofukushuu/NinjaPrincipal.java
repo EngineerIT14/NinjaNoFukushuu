158b56c3cadbebab46870df978ef4047a42a9ebe
Luis Espinosa <A01375758@itesm.mx>
2016-02-19 02:10:01 -0600

    ya

--- core/src/mx/itesm/ninjanofukushuu/NinjaPrincipal.java
+++ core/src/mx/itesm/ninjanofukushuu/NinjaPrincipal.java
@@ -1,6 +1,7 @@
 package mx.itesm.ninjanofukushuu;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -14,11 +15,13 @@ public class NinjaPrincipal {
     public World world;//cargamos el mundo
     public Body b2Body;//Creamos el cuerpo
     private Texture Ninja;
+    private Estado estado; //Reacionado con movimiento
 
     public NinjaPrincipal(World world){
         this.world=world;
         defineNinja();
         Ninja = new Texture(Gdx.files.internal("SN.png"));
+        estado = Estado.DERECHA;
     }
 
     public void defineNinja(){
@@ -33,4 +36,35 @@ public class NinjaPrincipal {
         fidef.shape=shape;
         b2Body.createFixture(fidef); //Crea el objeto con lo que definimos en Fixture.
     }
+
+    //Estado ninja, indica que hace
+    public void actualizar() {
+        switch (estado) {
+            case DERECHA:
+                b2Body.applyLinearImpulse(new Vector2(3f, 0), b2Body.getWorldCenter(), true);
+                break;
+            case IZQUIERDA:
+                b2Body.applyLinearImpulse(new Vector2(3f, 0), b2Body.getWorldCenter(), true);
+                break;
+            case SALTANDO:
+                b2Body.applyLinearImpulse(new Vector2(0, 20000), b2Body.getWorldCenter(), true);
+                break;
+        }
+    }
+
+    public void cambiarEstado(){
+
+    }
+
+
+    public void Saltar(){
+
+    }
+
+    public  enum  Estado{
+        DERECHA,
+        IZQUIERDA,
+        SALTANDO
+    }
+
 }
--- core/src/mx/itesm/ninjanofukushuu/PantallaJuego.java
+++ core/src/mx/itesm/ninjanofukushuu/PantallaJuego.java
@@ -14,12 +14,14 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -47,6 +49,18 @@ public class PantallaJuego implements Screen{
 
     private NinjaPrincipal jugador;
 
+    //Scrolls
+    private Array<ObjetosJuego> scroll;
+    private Texture texturaScroll;
+
+    //Vidas
+    private Array<ObjetosJuego> vidas;
+    private Texture texturaVidas;
+
+    //Pociones
+    private Array<ObjetosJuego> pociones;
+    private Texture texturaPocion;
+
     //creamos constructor por default
     public PantallaJuego(Principal principal){
         this.principal= principal;
@@ -69,9 +83,47 @@ public class PantallaJuego implements Screen{
         jugador = new NinjaPrincipal(world);
     }
 
+    private void crearObjetos() {
+        //Lista scrolles
+        scroll = new Array<ObjetosJuego>(3);
+        for (int i = 0; i<3;i++) {
+            ObjetosJuego nuevo = new ObjetosJuego(texturaScroll);
+            nuevo.setTamanio(10,10);
+            scroll.add(nuevo);
+        }
+        scroll.get(0).setPosicion(550,300);
+        scroll.get(1).setPosicion(800,50);
+        scroll.get(2).setPosicion(230,630);
+        //Vidas
+        vidas = new Array<ObjetosJuego>(3);
+        for(int i =0; i<3 ;i++){
+            ObjetosJuego nuevo = new ObjetosJuego(texturaVidas);
+            nuevo.setTamanio(50,50);
+            nuevo.setPosicion(30+i*50,650);
+            vidas.add(nuevo);
+        }
+        //Pociones
+        pociones = new Array<ObjetosJuego>(2);
+        for(int i =0; i< 2;i++){
+            ObjetosJuego nuevo = new ObjetosJuego(texturaVidas);
+            nuevo.setTamanio(10,10);
+            pociones.add(nuevo);
+        }
+        pociones.get(0).setPosicion(1000,Principal.ALTO_MUNDO/2);
+        pociones.get(1).setPosicion(300,500);
+    }
+
     @Override
     public void show() {
+        cargarTexturas();
         batch = new SpriteBatch();
+        crearObjetos();
+    }
+
+    private void cargarTexturas() {
+        texturaVidas = new Texture(Gdx.files.internal("life1.png"));
+        texturaScroll = new Texture(Gdx.files.internal("scroll.png"));
+        texturaPocion = new Texture(Gdx.files.internal("pocion.png"));
     }
 
     public void handleInput(float dt){
@@ -97,17 +149,65 @@ public class PantallaJuego implements Screen{
         update(delta);
 
         //Limpiar pantalla
-        Gdx.gl.glClearColor(60/255.0f, 181/255.0f, 0, .6f);
+        Gdx.gl.glClearColor(60 / 255.0f, 181 / 255.0f, 0, .6f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.setProjectionMatrix(camara.combined);  //Con este ajustas el batch..., si no, no va aparecer la imagen
         renderer.render();
         bodytodr.render(world, camara.combined);
-
+        recogerObjeto();
+        //jugador.actualizar();
+        //leerEntrada();
         principal.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
-
+        batch.begin();
+        for(ObjetosJuego scrolls:scroll) {
+            if(scrolls.actualizar())
+                scrolls.render(batch);
+        }
+        for (ObjetosJuego vida:vidas){
+            if(vida.actualizar())
+                vida.render(batch);
+        }
+        for (ObjetosJuego pocion:pociones){
+            if(pocion.actualizar())
+                pocion.render(batch);
+        }
+        batch.end();
     }
+    //Metodo para revomer elementos del juego
+    private void recogerObjeto() {
+        for (ObjetosJuego scrolls : scroll) {
+            if (jugador.b2Body.getPosition().x >= scrolls.getSprite().getX() && jugador.b2Body.getPosition().x <= scrolls.getSprite().getX() + scrolls.getSprite().getWidth() &&
+                    jugador.b2Body.getPosition().y >= scrolls.getSprite().getY() && jugador.b2Body.getPosition().y <= scrolls.getSprite().getY() + scrolls.getSprite().getHeight()) {
+                scrolls.quitarElemento();
+                scroll.pop();
+            }
+        }
+        for (ObjetosJuego pocion : pociones) {
+            if (jugador.b2Body.getPosition().x >= pocion.getSprite().getX() && jugador.b2Body.getPosition().x <= pocion.getSprite().getX() + pocion.getSprite().getWidth() &&
+                    jugador.b2Body.getPosition().y >= pocion.getSprite().getY() && jugador.b2Body.getPosition().y <= pocion.getSprite().getY() + pocion.getSprite().getHeight()) {
+                pocion.quitarElemento();
+                pociones.pop();
+                ObjetosJuego nuevo = new ObjetosJuego(texturaVidas);
+                nuevo.setTamanio(50,50);
+                nuevo.setPosicion(vidas.get(vidas.size-1).getSprite().getX()+50,650);
+                vidas.add(nuevo);
+            }
+        }
+    }
+    /*private void leerEntrada() {
+        if(Gdx.input.justTouched()){
+            Vector3 coordenadas = new Vector3();
+            coordenadas.set(Gdx.input.getX(),Gdx.input.getY(),0);
+            camara.unproject(coordenadas); //traduce las coordenadas
+            float x = coordenadas.x;
+            float y = coordenadas.y;
+            if (x >= 0 && x <= Principal.ANCHO_MUNDO && y >= 0 && y <= Principal.ALTO_MUNDO) {
+                jugador.Saltar();
+            }
+        }
+    }*/
 
     @Override
     public void resize(int width, int height) {
@@ -138,6 +238,9 @@ public class PantallaJuego implements Screen{
         renderer.dispose();
         world.dispose();
         bodytodr.dispose();
+        texturaPocion.dispose();
+        texturaScroll.dispose();
+        texturaVidas.dispose();
     }
 
 
