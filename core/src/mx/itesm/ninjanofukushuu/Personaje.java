4a4c6d017db86207bf376425dff041c485c590b9
Irvin Trujillo <irving_emmanuel95@hotmail.com>
2016-03-31 23:54:23 -0600

    //Nueva posicion de botones, ahora cuando le das mute la bocina cambia a silencio..

--- core/src/mx/itesm/ninjanofukushuu/Personaje.java
+++ core/src/mx/itesm/ninjanofukushuu/Personaje.java
@@ -42,10 +42,10 @@ public class Personaje
         // Lee la textura como región
         TextureRegion texturaCompleta = new TextureRegion(textura);
         // La divide en frames de 16x32 (ver marioSprite.png)
-        TextureRegion[][] texturaPersonajeCorriendo= texturaCompleta.split(16,32);
+        TextureRegion[][] texturaPersonajeCorriendo= texturaCompleta.split(18,32);
 
-        // Crea la animación con tiempo de 0.25 segundos entre frames.
-        animacion = new Animation(0.25f,texturaPersonajeCorriendo[0][1], texturaPersonajeCorriendo[0][2],  texturaPersonajeCorriendo[0][3],texturaPersonajeCorriendo[0][4],texturaPersonajeCorriendo[0][5],texturaPersonajeCorriendo[0][6],texturaPersonajeCorriendo[0][7] );  //La matriz [0][3] continene un sprite de hataku con el pie adelante y así.. va caminando
+        // Crea la animación con tiempo de 0.15 segundos entre frames.
+        animacion = new Animation(0.10f,texturaPersonajeCorriendo[0][1], texturaPersonajeCorriendo[0][2],  texturaPersonajeCorriendo[0][3],texturaPersonajeCorriendo[0][4],texturaPersonajeCorriendo[0][5],texturaPersonajeCorriendo[0][6]);  //La matriz [0][3] continene un sprite de hataku con el pie adelante y así.. va caminando
         // Animación infinita
         animacion.setPlayMode(Animation.PlayMode.LOOP);
         // Inicia el timer que contará tiempo para saber qué frame se dibuja
